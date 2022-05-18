58897a370811780d04e2bbb82fe0d3348c431c72
justina823 <laisumjustina.chan@my.jcu.edu.au>
2022-05-18 19:59:12 +1000

    add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { GoChevronLeft,GoChevronRight } from "react-icons/go";
+import { IoIosCheckmarkCircleOutline,IoIosCheckmarkCircle,IoIosAddCircleOutline } from "react-icons/io";
+const App = () => {
+	// HINT: each "item" in our list names a name, a boolean to tell if its been completed, and a quantity
+	const [items, setItems] = useState([
+        {itemName:'item 1', quantity:1, isSelected:false},
+        {itemName:'item 2', quantity:3, isSelected:true},
+        {itemName:'item 3', quantity:2, isSelected:false}
+
+    ]);
+    const [inputValue, setInputValue] = useState("")
+    const handleAddButtonClick =()=>{
+        alert("youuuuuu")
+    }
+
+	return (
+		<div className='app-background'>
+			<div className='main-container'>
+				<div className='add-item-box'>
+					<input value={inputValue} onChange={(event)=>
+                    setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+					<IoIosAddCircleOutline onClick={()=> handleAddButtonClick} />
+				</div>
+				<div className='item-list'>
+                    {items.map((item, index)=>
+                        <div className='item-container'>
+                        <div className='item-name'>
+                            {/* HINT: replace false with a boolean indicating the item has been completed or not */}
+                            {item.isSelected ? (
+                                <>
+                                    <IoIosCheckmarkCircle/>
+                                    <span className='completed'>{item.itemName}</span>
+                                </>
+                            ) : (
+                                <>
+                                    <IoIosCheckmarkCircleOutline />
+                                    <span>{item.itemName}</span>
+                                </>
+                            )}
+                        </div>
+                        <div className='quantity'>
+                            <button>
+                                <GoChevronLeft />
+                            </button>
+                            <span> {item.quantity} </span>
+                            <button>
+                                <GoChevronRight />
+                            </button>
+                        </div>
+                    </div>
+                    )}
+					
+				</div>
+				<div className='total'>Total: 6</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
